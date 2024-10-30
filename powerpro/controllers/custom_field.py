ca7c5c04da7251228f7ebaf6f40c0e5e1b7a8d0e
Yefri Tavarez <yefritavarez@gmail.com>
2024-10-30 00:51:14 -0400

    feature: Implement Custom Field class and update hooks for custom field handling

---
+++ powerpro/controllers/custom_field.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2024, Yefri Tavarez and Contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.utils import cstr, random_string
+
+from frappe.custom.doctype.custom_field import custom_field
+
+
+class CustomField(custom_field.CustomField):
+	def set_fieldname(self):
+		restricted = (
+			"name",
+			"parent",
+			"creation",
+			"modified",
+			"modified_by",
+			"parentfield",
+			"parenttype",
+			"file_list",
+			"flags",
+			"docstatus",
+		)
+		if not self.fieldname:
+			label = self.label
+			if not label:
+				if self.fieldtype in ["Section Break", "Column Break", "Tab Break"]:
+					label = self.fieldtype + "_" + str(random_string(5))
+				else:
+					frappe.throw(_("Label is mandatory"))
+
+			# remove special characters from fieldname
+			self.fieldname = "".join(
+				[c for c in cstr(label).replace(" ", "_") if c.isdigit() or c.isalpha() or c == "_"]
+			)
+			# self.fieldname = f"{self.fieldname}"
+
+		# fieldnames should be lowercase
+		self.fieldname = self.fieldname.lower()
+
+		if self.fieldname in restricted:
+			self.fieldname = self.fieldname + "1"
