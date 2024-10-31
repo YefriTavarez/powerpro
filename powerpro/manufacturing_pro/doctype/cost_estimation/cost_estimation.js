d88f72a1c1ede04bba8ba6f9538b7cde75ab21e2
Unknown Developer <unknown@sonofamother.fuck>
2024-10-30 22:41:20 -0400

    feature: Add autoset and validation for expires_on field in Cost Estimation

--- powerpro/manufacturing_pro/doctype/cost_estimation/cost_estimation.js
+++ powerpro/manufacturing_pro/doctype/cost_estimation/cost_estimation.js
@@ -31,6 +31,41 @@
 		});
 	}
 
+	function _autoset_expires_on({ frm, force = false }) {
+		// sets the expires on based on the created_on (if set)
+		// otherwise, sets the expires on based on the current date + 30 days
+		// if not created_on is set and created_on is set and force is false, then do nothing
+		// if created_on is set and force is true, then set the expires_on based on the created_on
+
+		const { doc } = frm;
+		const { add_days } = frappe.datetime;
+
+		if (!doc.created_on) {
+			if (force) {
+				doc.expires_on = add_days(frappe.datetime.now_date(), 30);
+			}
+		} else {
+			doc.expires_on = add_days(doc.created_on, 30);
+		}
+
+		frm.refresh_field("expires_on");
+	}
+
+	function _validate_expires_on(frm) {
+		// validate expires_on is never less than created_on (if set)
+		// if created_on is not set, then let's make sure it is not less than the current date
+
+		const { doc } = frm;
+
+		if (!doc.created_on) {
+			if (doc.expires_on < frappe.datetime.now_date()) {
+				frappe.msgprint(__("Expires On cannot be less than the current date!"));
+			}
+		} else if (doc.expires_on < doc.created_on) {
+			frappe.msgprint(__("Expires On cannot be less than Created On!"));
+		}
+	}
+
 	function setup(frm) {
 		_setup_vue(frm);
 	}
@@ -40,8 +75,24 @@
 		cost_estimation.fetch_raw_material_specs();
 	}
 
+	function onload_post_render(frm) {
+		_autoset_expires_on({ frm });
+	}
+
+	function created_on(frm) {
+		// update expires_on
+		_autoset_expires_on({ frm, force: true });
+	}
+
+	function expires_on(frm) {
+		_validate_expires_on(frm);
+	}
+
 	frappe.ui.form.on("Cost Estimation", {
 		setup,
+		onload_post_render,
 		raw_material,
+		created_on,
+		expires_on,
 	});
 }
