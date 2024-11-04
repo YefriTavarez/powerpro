74bec8bcff6ebb9d87904454cab85e59715cc424
Unknown Developer <unknown@sonofamother.fuck>
2024-11-04 01:33:45 -0500

    feature: Implement rate_per_kg calculation and remove zero percentage lines in Ink Color doctype

--- powerpro/manufacturing_pro/doctype/ink_color/ink_color.py
+++ powerpro/manufacturing_pro/doctype/ink_color/ink_color.py
@@ -6,11 +6,14 @@ from typing import List, Literal, TYPE_CHECKING
 import frappe
 from frappe.model.document import Document
 
+from frappe.utils import flt
+
 if TYPE_CHECKING:
 	from powerpro.manufacturing_pro.doctype.pantone_composition import pantone_composition
 
 class InkColor(Document):
 	def validate(self):
+		self.remove_zero_percentage_lines()
 		self.ensure_100_percent_pantone_composition()
 		self.clear_pantone_composition_if_applies()
 		self.clear_pantone_type_if_applies()
@@ -30,6 +33,29 @@ class InkColor(Document):
 	def update_rate_per_kg_if_applies(self):
 		# if the rate_per_kg is based on formula, let's then calculate it
 		# here.
+		if self.ink_type == "Pantone" \
+			and self.pantone_type == "Formula":
+			self.rate_per_kg = self.calculate_rate_per_kg()
+
+	def calculate_rate_per_kg(self):
+		out = 0.000
+
+		for line_item in self.pantone_composition:
+			out += flt(line_item.percentage) * line_item.rate_per_kg / 100
+		
+		return flt(out, 3)
+
+	def remove_zero_percentage_lines(self):
+		# remove all the lines with 0 percentage
+		self.pantone_composition = [
+			item
+			for item in self.pantone_composition
+			if item.get("percentage", 0) > 0
+		]
+
+		# recalculate idx
+		for idx, item in enumerate(self.pantone_composition):
+			item.idx = idx + 1
 
 	def ensure_100_percent_pantone_composition(self):
 		if self.ink_type == "Pantone" \
