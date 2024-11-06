dfd3bd40007a898a6b662c32bd722d05d7954aa1
Miguel Higuera <migueladolfohiguera@hotmail.com>
2024-11-05 23:44:49 -0500

    IGC-16 feature: DocType > Reporte 623 - Generating Report 623

--- powerpro/power_pro/doctype/reporte_623/reporte_623.js
+++ powerpro/power_pro/doctype/reporte_623/reporte_623.js
@@ -3,17 +3,17 @@
 
 
 frappe.ui.form.on("Reporte 623", {
-	onload: function(frm) {
-		frm.set_value("from_date", frappe.datetime.month_start());
-		frm.set_value("to_date", frappe.datetime.month_end());
-		frm.disable_save();
-	},
-	download: function(frm){
-		var file_url = __("/api/method/powerpro.power_pro.doctype.reporte_623.reporte_623.get_file_address?from_date={0}&to_date={1}", 
-			[frm.doc.from_date, frm.doc.to_date]);
+    onload: function(frm) {
+        frm.set_value("from_date", frappe.datetime.month_start());
+        frm.set_value("to_date", frappe.datetime.month_end());
+        frm.disable_save();
+    },
+    download: function(frm){
+        var file_url = __("/api/method/powerpro.power_pro.doctype.reporte_623.reporte_623.get_file_address?from_date={0}&to_date={1}", 
+            [frm.doc.from_date, frm.doc.to_date]);
 
-		window.open(file_url);
-	}
+        window.open(file_url);
+    }
 });
 
 
--- powerpro/power_pro/doctype/reporte_623/reporte_623.py
+++ powerpro/power_pro/doctype/reporte_623/reporte_623.py
@@ -12,71 +12,71 @@ import time
 
 
 class Reporte623(Document):
-	pass
+    pass
 
 
 @frappe.whitelist()
 def get_file_address(from_date, to_date):
-	data = f"""
-		Select 
-			supplier.tax_id,
-			purchase_invoice.posting_date,
-			purchase_invoice.retention_date,
-			purchase_invoice.retention_amount,
-			payment_entry.reference_no,
-			payment_entry.mode_of_payment,
-			bank_account.bank,
-			purchase_invoice.status
-		From
-			`tabPurchase Invoice` As purchase_invoice
-		Inner Join
-			`tabSupplier` As supplier
-		On
-			purchase_invoice.supplier = supplier.name
-		Inner Join
-			`tabPayment Entry Reference` As payment_entry_reference
-		On
-			purchase_invoice.name = payment_entry_reference.reference_name
-		Inner Join
-			`tabPayment Entry` As payment_entry
-		On
-			payment_entry_reference.parent = payment_entry.name
-		Inner Join
-			`tabBank Account` As bank_account
-		On
-			payment_entry.bank_account = bank_account.name
-		Where 
-			purchase_invoice.docstatus = 1 And
-			purchase_invoice.posting_date Between {from_date!r} And {to_date!r}
-		"""
-	result = frappe.db.sql(data, as_dict=True)
+    data = f"""
+        Select 
+            supplier.tax_id,
+            purchase_invoice.posting_date,
+            purchase_invoice.retention_date,
+            purchase_invoice.retention_amount,
+            payment_entry.reference_no,
+            payment_entry.mode_of_payment,
+            bank_account.bank,
+            purchase_invoice.status
+        From
+            `tabPurchase Invoice` As purchase_invoice
+        Inner Join
+            `tabSupplier` As supplier
+        On
+            purchase_invoice.supplier = supplier.name
+        Inner Join
+            `tabPayment Entry Reference` As payment_entry_reference
+        On
+            purchase_invoice.name = payment_entry_reference.reference_name
+        Inner Join
+            `tabPayment Entry` As payment_entry
+        On
+            payment_entry_reference.parent = payment_entry.name
+        Inner Join
+            `tabBank Account` As bank_account
+        On
+            payment_entry.bank_account = bank_account.name
+        Where 
+            purchase_invoice.docstatus = 1 And
+            purchase_invoice.posting_date Between {from_date!r} And {to_date!r}
+        """
+    result = frappe.db.sql(data, as_dict=True)
 
-	w = UnicodeWriter()
-	w.writerow([
-		"RNC Entidad del Estado",
-		"Período",
-		"Fecha de retención",
-		"Valor de retención",
-		"Número de Referencia",
-		"Tipo de referencia", # Get the doctype name
-		"Banco",
-		"Estado",
-	])
+    w = UnicodeWriter()
+    w.writerow([
+        "RNC Entidad del Estado",
+        "Período",
+        "Fecha de retención",
+        "Valor de retención",
+        "Número de Referencia",
+        "Tipo de referencia", # Get the doctype name
+        "Banco",
+        "Estado",
+    ])
 
-	for row in result:
-		# Dates in the format YYYYMM
-		w.writerow([
-			row.tax_id,
-			format_datetime(row.posting_date, "yyyyMM"),
-			format_datetime(row.retention_date, "yyyyMMdd"),
-			row.retention_amount,
-			row.reference_no,
-			row.mode_of_payment,
-			row.bank,
-			"En Proceso" if row.status == "Draft" else "Completada" if row.status == "Paid" else "Activa"
-		])
+    for row in result:
+        # Dates in the format YYYYMM
+        w.writerow([
+            row.tax_id,
+            format_datetime(row.posting_date, "yyyyMM"),
+            format_datetime(row.retention_date, "yyyyMMdd"),
+            row.retention_amount,
+            row.reference_no,
+            row.mode_of_payment,
+            row.bank,
+            "En Proceso" if row.status == "Draft" else "Completada" if row.status == "Paid" else "Activa"
+        ])
 
-	frappe.response["result"] = cstr(w.getvalue())
-	frappe.response["type"] = "csv"
-	frappe.response["doctype"] = "Reporte_623_" + str(int(time.time()))
+    frappe.response["result"] = cstr(w.getvalue())
+    frappe.response["type"] = "csv"
+    frappe.response["doctype"] = "Reporte_623_" + str(int(time.time()))
 
