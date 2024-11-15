eed55cf76892cee2e6cc8987cdd9aa8e8d808ce9
Yefri Tavarez <yefritavarez@gmail.com>
2024-11-15 11:31:19 +0000

    feat: Enhance QualityProcedure with version bump methods and internal save functionality

--- powerpro/controllers/quality_procedure.py
+++ powerpro/controllers/quality_procedure.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2024, Yefri Tavarez and Contributors
 # For license information, please see license.txt
 
+from typing import Optional
+
 import frappe
 
 from semver import Version
@@ -16,38 +18,60 @@ class QualityProcedure(quality_procedure.QualityProcedure):
 		self.name = naming.make_autoname(naming_series)
 
 	def validate(self):
+		if self.is_new():
+			self.revision = "1.0.0"
+			return # ignore new documents
+
 		if not self.flags.internal_save:
 			frappe.throw(
 				_("You can't update directly a Quality Procedure. Use the buttons instead.")
 			)
 
 	@frappe.whitelist()
-	def bump_major(self, autosave: bool = False):
+	def bump_major(self, autosave: bool = False) -> Optional[str]:
 		version = self.get_version()
 		self.revision = str(version.bump_major())
 
 		if autosave:
-			self.db_set("revision", self.revision)
+			return self.supersede()
 
 	@frappe.whitelist()
-	def bump_minor(self, autosave: bool = False):
+	def bump_minor(self, autosave: bool = False) -> Optional[str]:
 		version = self.get_version()
 		self.revision = str(version.bump_minor())
 
 		if autosave:
-			self.db_set("revision", self.revision)
+			return self.supersede()
 
 	@frappe.whitelist()
-	def bump_patch(self, autosave: bool = False):
+	def bump_patch(self, autosave: bool = False) -> Optional[str]:
 		version = self.get_version()
 		self.revision = str(version.bump_patch())
 
 		if autosave:
-			self.db_set("revision", self.revision)
+			return self.supersede()
 
+	@frappe.whitelist()
 	def save_only(self):
 		self.flags.internal_save = True
 		self.save()
+
+	def supersede(self) -> None:
+		self.db_set("status", "Superseded")
+
+		copy = frappe.copy_doc(self)
+		copy.revision = self.revision
+		copy.status = "Published"
+		copy.closest_seed = self.name
+
+		if not copy.seed:
+			copy.seed = copy.closest_seed
+
+		copy.flags.ignore_permissions = True
+		copy.flags.internal_save = True
+		copy.save()
+
+		return copy.name
 
 	def get_version(self) -> Version:
 		return Version.parse(self.revision)
