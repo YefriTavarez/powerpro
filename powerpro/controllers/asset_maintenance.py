24d96f65461f801a1cf6a92388a36368be06b877
Miguel Higuera <miguel.higuera@tzcode.tech>
2024-11-22 11:02:34 -0500

    IGC-17 refactor

--- powerpro/controllers/asset_maintenance.py
+++ powerpro/controllers/asset_maintenance.py
@@ -14,8 +14,6 @@ from erpnext.assets.doctype.asset_maintenance.asset_maintenance import AssetMain
 
 class AssetMaintenance(AssetMaintenance):
     def on_update(self):
-        for task in self.get("asset_maintenance_tasks"):
-            assign_tasks(task.name, task.assign_to, task.maintenance_task, task.next_due_date, self.item_code, self.item_name)
         self.sync_maintenance_tasks()
 
     def sync_maintenance_tasks(self):
@@ -64,55 +62,54 @@ class AssetMaintenance(AssetMaintenance):
         return frappe.get_all(doctype, filters=filters)
 
 
-def assign_tasks(asset_maintenance_name, assign_to_member, maintenance_task, next_due_date, item_code, item_name):
-    team_member = frappe.db.get_value("User", assign_to_member, "email")
+# def assign_tasks(asset_maintenance_name, assign_to_member, maintenance_task, next_due_date, item_code, item_name):
+#     team_member = frappe.db.get_value("User", assign_to_member, "email")
 
-    args = {
-        "doctype": "Asset Maintenance Task",
-        "assign_to": team_member,
-        "name": asset_maintenance_name,
-        "description": maintenance_task,
-        "date": next_due_date,
-        "item_code": item_code,
-        "item_name": item_name,
-    }
+#     args = {
+#         "doctype": "Asset Maintenance Task",
+#         "assign_to": team_member,
+#         "name": asset_maintenance_name,
+#         "description": maintenance_task,
+#         "date": next_due_date,
+#         "item_code": item_code,
+#         "item_name": item_name,
+#         "asset_name": asset_maintenance_name,
+#     }
 
-    # Busca un ToDo abierto para la misma tarea (por description) asignado a otro usuario
-    existing_todo = frappe.db.get_value(
-        "ToDo",
-        {
-            "reference_type": args["doctype"],
-            "reference_name": args["name"],
-            "description": args["description"],
-            "status": "Open",
-            "allocated_to": ("!=", team_member),
-        },
-        "name",  # Devuelve el nombre del ToDo
-    )
+#     existing_todo = frappe.db.get_value(
+#         "ToDo",
+#         {
+#             "reference_type": args["doctype"],
+#             "reference_name": args["name"],
+#             "description": args["description"],
+#             "status": "Open",
+#             "allocated_to": ("!=", team_member),
+#         },
+#         "name",
+#     )
 
-    # Cancela el ToDo existente si está asignado a otro usuario
-    if existing_todo:
-        frappe.db.set_value("ToDo", existing_todo, "status", "Cancelled")
+#     if existing_todo:
+#         frappe.db.set_value("ToDo", existing_todo, "status", "Cancelled")
 
-    # Verifica si ya existe un ToDo abierto para este usuario con esta descripción
-    todo_exists = frappe.db.exists(
-        "ToDo",
-        {
-            "reference_type": args["doctype"],
-            "reference_name": args["name"],
-            "description": args["description"],
-            "allocated_to": team_member,
-            "status": "Open",
-        },
-    )
+#     todo_exists = frappe.db.exists(
+#         "ToDo",
+#         {
+#             "reference_type": args["doctype"],
+#             "reference_name": args["name"],
+#             "description": args["description"],
+#             "allocated_to": team_member,
+#             "status": "Open",
+#         },
+#     )
 
-    # Si no existe, crea uno nuevo
-    if not todo_exists:
-        args["assign_to"] = [args["assign_to"]]
-        assign_to.add(args, ignore_permissions=True) 
+#     if not todo_exists:
+#         args["assign_to"] = [args["assign_to"]]
+#         assign_to.add(args) 
+
 
 
 def update_maintenance_log(asset_maintenance, item_code, item_name, task):
+    asset_maintenance_doc = get_asset_maintenance(asset_maintenance)
     asset_maintenance_log = frappe.get_value(
         "Asset Maintenance Log",
         {
@@ -138,9 +135,13 @@ def update_maintenance_log(asset_maintenance, item_code, item_name, task):
                 "periodicity": str(task.periodicity),
                 "maintenance_type": task.maintenance_type,
                 "due_date": task.next_due_date,
+                "maintenance_team": asset_maintenance_doc.maintenance_team,
+                "maintenance_manager": asset_maintenance_doc.maintenance_manager,
+                "maintenance_manager_name": asset_maintenance_doc.maintenance_manager_name,
             }
         )
         asset_maintenance_log.flags.ignore_permissions = True
+        asset_maintenance_log.flags.ignore_validate_update_after_submit = True
         asset_maintenance_log.insert()
     else:
         maintenance_log = frappe.get_doc("Asset Maintenance Log", asset_maintenance_log)
@@ -150,8 +151,41 @@ def update_maintenance_log(asset_maintenance, item_code, item_name, task):
         maintenance_log.description = task.description
         maintenance_log.periodicity = str(task.periodicity)
         maintenance_log.maintenance_type = task.maintenance_type
-        maintenance_log.due_date = task.next_due_date
+        maintenance_log.maintenance_team = asset_maintenance_doc.maintenance_team
+        maintenance_log.maintenance_manager = asset_maintenance_doc.maintenance_manager
+        maintenance_log.maintenance_manager_name = asset_maintenance_doc.maintenance_manager_name
+
+        if not maintenance_log.flags.override_due_date:
+            maintenance_log.due_date = task.next_due_date
 
         maintenance_log.flags.ignore_permissions = True
+        maintenance_log.flags.ignore_validate_update_after_submit = True
         maintenance_log.save()
 
+
+@frappe.whitelist()
+def submit_logs(asset_maintenance):
+    asset_maintenance_doc = get_asset_maintenance(asset_maintenance)
+
+    for task in asset_maintenance_doc.get("asset_maintenance_tasks"):
+        get_planned_maintenance_log(asset_maintenance, task.name, task.maintenance_status, task.next_due_date)
+
+
+def get_planned_maintenance_log(asset_maintenance, task, maintenance_status, due_date):
+    doctype = "Asset Maintenance Log"
+    filters = {
+        "asset_maintenance": asset_maintenance,
+        "task": task,
+        "maintenance_status": ("in", ["Planned", "Overdue"]),
+        "due_date": due_date,
+    }
+
+    if name := frappe.db.exists(doctype, filters):
+        doc = frappe.get_doc(doctype, name)
+        doc.ignore_permissions = True
+        doc.submit()
+
+
+def get_asset_maintenance(asset_maintenance):
+    doctype = "Asset Maintenance"
+    return frappe.get_doc(doctype, asset_maintenance)
--- powerpro/controllers/todo.py
+++ powerpro/controllers/todo.py
@@ -1,4 +1,5 @@
 import frappe
+from erpnext.assets.doctype.asset_maintenance.asset_maintenance import calculate_next_due_date
 
 
 @frappe.whitelist()
@@ -19,10 +20,58 @@ def close_maintenance_task(todo, task, due_date):
     maintenance_log.update({
         "completion_date": frappe.utils.nowdate(),
         "actions_performed": todo.actions_performed,
+        "completed_by": frappe.session.user,
         "maintenance_status": "Completed",
     })
     maintenance_log.flags.ignore_permissions = True
-    maintenance_log.submit()
+    maintenance_log.flags.ignore_validate_update_after_submit = True
+    maintenance_log.save()
+
+    if not todo.is_old_maintenance:
+        create_next_maintenance_log(maintenance_log)
+
+
+def create_next_maintenance_log(log):
+    asset_maintenance = get_asset_maintenance(log.asset_maintenance)
+
+    due_date = calculate_next_due_date(
+        periodicity=log.periodicity,
+        start_date=log.due_date,
+    )
+
+    asset_maintenance = get_asset_maintenance(log.asset_maintenance)
+
+    for task in asset_maintenance.asset_maintenance_tasks:
+        if task.name == log.task:
+            task.update({
+                "next_due_date": due_date,
+                "maintenance_status": "Planned",
+            })
+
+    asset_maintenance.flags.ignore_permissions = True
+    asset_maintenance.save()
+
+
+@frappe.whitelist()
+def reopen_maintenance_task(todo, task, due_date):
+    todo = get_todo(todo)
+    todo.update({
+        "status": "Open",
+        "is_old_maintenance": True,
+    })
+    todo.flags.ignore_permissions = True
+    todo.save()
+
+    log = get_completed_asset_maintenance_log(task, due_date)
+    log.update({
+        "completion_date": None,
+        "actions_performed": None,
+        "maintenance_status": "Planned",
+        "completed_by": None,
+    })
+    log.flags.ignore_permissions = True
+    log.db_update()
+
 
 
 def get_todo(todo):
@@ -38,4 +87,19 @@ def get_asset_maintenance_log(task, due_date):
         "maintenance_status": ("in", ["Planned", "Overdue"]),
     }
     return frappe.get_doc(doctype, filters)
+
+
+def get_completed_asset_maintenance_log(task, due_date):
+    doctype = "Asset Maintenance Log"
+    filters = {
+        "task": task,
+        "due_date": due_date,
+        "maintenance_status": "Completed",
+    }
+    return frappe.get_doc(doctype, filters)
+
+
+def get_asset_maintenance(asset_maintenance):
+    doctype = "Asset Maintenance"
+    return frappe.get_doc(doctype, asset_maintenance)
 
