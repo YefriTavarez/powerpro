7874bda4fb1ea2f8f3ae7c82b56de7e25cdf567f
Yefri Tavarez <yefritavarez@gmail.com>
2024-11-24 02:22:55 +0000

    feat: Enhance raw material query function with improved filtering and search conditions

--- powerpro/controllers/queries.py
+++ powerpro/controllers/queries.py
@@ -2,50 +2,61 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 
 
 @frappe.whitelist()
 def raw_material_query(doctype, txt, searchfield, start, page_len, filters):
+	"""Return raw materials based on the search criteria"""
+
 	if filters is None:
 		filters = {}
 
 	if isinstance(filters, list):
 		frappe.throw(
-			"Filters must be a dictionary"
+			_("Filters should be a dictionary")
 		)
 
+
 	if product_type := filters.get("product_type"):
-		del filters["product_type"]
+		text_conditions = get_txt_conditions(txt)
+		return frappe.db.sql(
+			f"""
+				Select
+					material.name,
+					material.description
+				From
+					`tabRaw Material` As material
+				Where
+					material.name In (
+						Select
+							child.raw_materials
+						From
+							`tabRaw Materials` As child
+						Where
+							child.parenttype = "Product Type"
+							And child.parentfield = "raw_materials"
+							And child.parent = {product_type!r}
+					) {text_conditions} 
+			"""
+		)
+	else:
+		if txt:
+			filters.update({
+				searchfield: ["like", f"'%{txt}%'"]
+			})
+		frappe.get_all(
+			"Raw Material",
+			filters=filters,
+			fields=["name", "description"],
+			as_list=True
+		)
+	
 
-		filters.update({
-			"parent": product_type,
-		})
-
+def get_txt_conditions(txt):
 	if txt:
-		filters.update({
-			"raw_materials": ["like", f"%{txt}%"],
-		})
+		return f"""
+			And material.description like "%{txt}%"
+		"""
 
-	materials_map = get_materials_map()
-	return [(d[0], materials_map[d[0]]) for d in frappe.get_all(
-		"Raw Materials",
-		filters=filters,
-		fields=["raw_materials"],
-		as_list=True,
-	)]
-
-
-def get_materials_map() -> dict:
-	doctype = "Raw Material"
-	filters = {
-		
-	}
-
-	fields = [
-		"name",
-		"description",
-	]
-
-	return dict(
-		frappe.get_all(doctype, filters, fields, as_list=True)
-	)
+	return ""
