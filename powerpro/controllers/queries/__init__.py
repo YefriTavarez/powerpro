7066be89710d5d16056c3a450369012b9e6f62d2
Yefri Tavarez <yefritavarez@gmail.com>
2024-11-30 17:16:11 -0500

    ref: convert queries python controller to a package with its own sub-modules

---
+++ powerpro/controllers/queries/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Yefri Tavarez and Contributors
+# For license information, please see license.txt
+
+__all__ = (
+	"raw_material_query",
+)
+
+from powerpro.controllers.queries.raw_material import raw_material_query
