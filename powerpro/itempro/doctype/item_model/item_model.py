0ab78a192e05427e9f3d0a0d3e5651e6f81ca5e0
Yefri Tavarez <yefritavarez@gmail.com>
2024-12-02 21:33:15 -0500

    feat: add type hints for item model fields in auto-generated section

--- powerpro/itempro/doctype/item_model/item_model.py
+++ powerpro/itempro/doctype/item_model/item_model.py
@@ -6,4 +6,18 @@ from frappe.model.document import Document
 
 
 class ItemModel(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from powerpro.itempro.doctype.item_brands.item_brands import ItemBrands
+		from powerpro.itempro.doctype.item_name_link.item_name_link import ItemNameLink
+
+		item_brands: DF.TableMultiSelect[ItemBrands]
+		item_model: DF.Data | None
+		item_names: DF.Table[ItemNameLink]
+	# end: auto-generated types
 	pass
