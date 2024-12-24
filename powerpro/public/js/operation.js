8fed7ea248dfb3172f85e2ac29a4d0dee388bfdf
Yefri Tavarez <yefritavarez@gmail.com>
2024-12-23 19:43:32 -0500

    feat: Implement dynamic filtering for workstation selection based on operation type

--- powerpro/public/js/operation.js
+++ powerpro/public/js/operation.js
@@ -3,8 +3,50 @@
 /* eslint-disable */
 
 {
-	// ToDo: Set filters to default_workstation field to only show Workstations
-	// compatible with the selected Operation Type
+	function refresh(frm) {
+		frm.set_query("workstation", function() {
+			return {
+				query: "powerpro.controllers.queries.get_workstation",
+				filters: {
+					"operation_type": frm.doc.operation_type,
+				},
+			};
+		});
+		frm.set_query("available_workstations", function() {
+			return {
+				query: "powerpro.controllers.queries.get_workstation",
+				filters: {
+					"operation_type": frm.doc.operation_type,
+				},
+			};
+		});
+	}
 
-	// ToDo: Enforce this rule.
+	function workstation(frm) {
+		const { doc } = frm;
+
+		if (!doc.workstation) {
+			return ; // only if workstation is set
+		}
+
+		// let's add the workstation to the available_workstations table,
+		// but only if it's not already there
+		const available_workstations = doc.available_workstations || [];
+		const is_already_added = available_workstations.some(
+			({ workstation }) => workstation === doc.workstation
+		);
+
+		if (!is_already_added) {
+			frm.add_child("available_workstations", {
+				workstation: doc.workstation,
+			});
+
+			frm.refresh_field("available_workstations");
+		}
+	}
+
+	frappe.ui.form.on("Operation", {
+		refresh,
+		workstation,
+	});
 }
