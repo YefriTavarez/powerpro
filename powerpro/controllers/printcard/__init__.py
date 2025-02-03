919fc74c6f3555e087fb1650d366c1a460eb55db
Yefri Tavarez <yefritavarez@gmail.com>
2025-02-02 23:01:43 -0500

    feat: Add initial printcard controller with necessary imports for PDF generation

---
+++ powerpro/controllers/printcard/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, Yefri Tavarez and Contributors
+# For license information, please see license.txt
+
+from .helper import (
+	generate_pdf_for_printcard,
+	get_file_path,
+	get_ink_color,
+	get_constrast_of_ink_color,
+	get_contrast,
+	get_canvas_list_without_ancho_specs,
+	get_minimum_canvas_margin,
+	get_best_canvas,
+	sign_pdf_with_base64,
+	get_princard,
+	get_canvas,
+	get_unique_filename,
+	convert_hex_to_rgb,
+)
+
+from .printcard import PrintCard
