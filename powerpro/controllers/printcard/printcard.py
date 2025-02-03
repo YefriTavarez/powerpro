ea7cf4455fba7e8af185c929f733490e0a6eccf8
Yefri Tavarez <yefritavarez@gmail.com>
2025-02-03 08:49:35 -0500

    feat: Refactor PrintCard class to enhance arte status management and PDF generation

--- powerpro/controllers/printcard/printcard.py
+++ powerpro/controllers/printcard/printcard.py
@@ -5,15 +5,36 @@
 import frappe
 from frappe.model.document import Document
 
+from powerpro.controllers.printcard import generate_pdf_for_printcard
+
 class PrintCard(Document):
     def before_save(self):
         self.update_arte_status()
         self.update_arte_fields()
         self.update_arte_changes()
+        self.save_arte()
 
-    def update_arte_status(self):
-        # Verificar si el PrintCard es el último
-        latest_version = frappe.db.sql(
+    def _get_arte(self):
+        if not hasattr(self, "_arte"):
+            _arte = frappe.get_doc("Arte", self.codigo_arte)
+            setattr(self, "_arte", _arte)
+        
+        return getattr(self, "_arte")
+    
+
+    @frappe.whitelist()
+    def get_lastest_version_of_printcard(self, related_arte: str) -> str:
+        """
+        Retrieves the latest version of a print card related to a specific arte.
+
+        Args:
+            related_arte (str): The identifier of the related arte.
+
+        Returns:
+            str: The latest version of the print card in the format "version.version_arte_interna".
+                 If no version is found, returns "0.0".
+        """
+        result = frappe.db.sql(
             f"""
             Select
                 Max(
@@ -26,45 +47,58 @@ class PrintCard(Document):
             From
                 `tabPrintCard`
             Where
-                codigo_arte = {self.codigo_arte!r}
+                codigo_arte = {related_arte!r}
             """
         )
 
-        arte = frappe.get_doc("Arte", self.codigo_arte)
+        if result:
+            return result[0][0]
 
-        # Actualizar el estado del Arte si el estado del PrintCard cambia a "Pendiente"
-        if self.estado == "Pendiente":
-            arte.estado = "Pendiente"
-            arte.flags.ignore_permissions = True
-            arte.flags.ignore_mandatory = True
-            arte.save()
+        return "0.0"
+
+    def is_latest_version(self):
+        return self.get_lastest_version_of_printcard(self.codigo_arte) == f"{self.version}.{self.version_arte_interna}"
+
+    def mark_as_replaced_previous_printcards(self):
+        if self.estado == "Aprobado":
+            for name in frappe.get_all("PrintCard", {
+                "codigo_arte": self.codigo_arte,
+                "estado": [
+                    "not in", ["Reemplazado", "Rechazado"],
+                ],
+                "name": ["!=", self.name],
+            }, pluck="name"):
+                frappe.db.set_value("PrintCard", name, "estado", "Reemplazado")
+
+    def update_arte_status(self):
+        arte = self._get_arte()
+
+        # # Actualizar el estado del Arte si el estado del PrintCard cambia a "Pendiente"
+        # if self.estado == "Pendiente":
+        #     arte.estado = "Pendiente"
+        #     arte.flags.ignore_permissions = True
+        #     arte.flags.ignore_mandatory = True
+        #     arte.save()
 
         # Si estamos en la última versión, aplicar lógica existente
-        if f"{self.version_arte_interna}.{self.version}" == latest_version[0][0]:
-            arte.estado = self.estado if self.estado not in {"Reemplazado"} else "Pendiente"
+        if self.is_latest_version():
+            arte.estado = self.estado
 
+            # these fields are only to display the latest info on the approved version
+            # in other words, this set of fields are only for the approved version.
             if self.estado == "Aprobado":
                 arte.ultima_version_aprobada = self.version_arte_interna
                 arte.producto_aprobado = self.producto
-                arte.estado_últ_aprobada = self.estado if self.estado not in {"Reemplazado"} else "Pendiente"
+                arte.estado_últ_aprobada = self.estado
                 arte.versión_interna_del_aprobada = arte.versión_del_cliente
 
-            arte.flags.ignore_permissions = True
-            arte.flags.ignore_mandatory = True
-            arte.save()
+            self.mark_as_replaced_previous_printcards()
 
-            if self.estado == "Aprobado":
-                for name in frappe.get_all("PrintCard", {
-                    "codigo_arte": self.codigo_arte,
-                    "name": ["!=", self.name],
-                }, pluck="name"):
-                    frappe.db.set_value("PrintCard", name, "estado", "Reemplazado")
-
-            frappe.msgprint(f"El Arte {arte.name} ha sido actualizada satisfactoriamente.", alert=True)
+            # frappe.msgprint(f"El Arte {arte.name} ha sido actualizada satisfactoriamente.", alert=True)
 
             db_doc = self.get_doc_before_save()
 
-            if db_doc.estado != self.estado:
+            if db_doc and db_doc.estado != self.estado:
                 # Actualizar tipo de cambio en el Arte
                 if self.estado == "Aprobado":
                     for row in arte.cambios:
@@ -93,15 +127,16 @@ class PrintCard(Document):
                                     row.db_update()
                                     break
 
-                    arte.db_set("estado", "Aprobado")
+                    # arte.db_set("estado", "Aprobado")
                 elif self.estado == "Rechazado":
                     # Revertir a la versión previa si está rechazada
-                    ultima_version_aprobada = frappe.db.get_value("PrintCard", {
-                        "codigo_arte": self.codigo_arte,
-                        "estado": "Aprobado",
-                    }, "Max(version)") or 0
-                    arte.db_set("ultima_version_aprobada", ultima_version_aprobada)
+                    # ultima_version_aprobada = frappe.db.get_value("PrintCard", {
+                    #     "codigo_arte": self.codigo_arte,
+                    #     "estado": "Aprobado",
+                    # }, "Max(version)") or 0
+                    # arte.db_set("ultima_version_aprobada", ultima_version_aprobada)
 
+                    # Update the status of the changes
                     for row in arte.cambios:
                         if row.printcard == self.name:
                             row.tipo_de_cambio = "PrintCard Rechazado"
@@ -114,10 +149,13 @@ class PrintCard(Document):
                             row.db_update()
                             break
 
-                    arte.db_set("estado", "Rechazado")
+                    # arte.db_set("estado", "Rechazado")
+                if self.estado not in {"Borrador",}:
+                # if it's the latest version, the status of the arte should be the same as the printcard
+                    arte.db_set("estado", self.estado)
 
     def update_arte_fields(self):
-        arte = frappe.get_doc("Arte", self.codigo_arte)
+        arte = self._get_arte()
 
         # Verificar y actualizar el campo "producto" si hay cambios
         if self.get_doc_before_save().producto != self.producto:
@@ -131,28 +169,33 @@ class PrintCard(Document):
         if self.get_doc_before_save().version_arte_cliente != self.version_arte_cliente:
             arte.versión_del_cliente = self.version_arte_cliente
 
-        arte.flags.ignore_permissions = True
-        arte.flags.ignore_mandatory = True
-        arte.save()
-
     def update_arte_changes(self):
-        arte = frappe.get_doc("Arte", self.codigo_arte)
+        arte = self._get_arte()
 
         db_doc = self.get_doc_before_save()
 
+        # PrintCard has been submitted for Approval (from Draft)
+        # at this point we should generate the PDF for the PrintCard
         if db_doc.estado == "Borrador" and self.estado == "Pendiente":
-            pdf_path = frappe.call(
-                "powerpro.controllers.printcard.generate_pdf_for_printcard",
-                printcard=self.name,
-                pdf_path=True
-            )
-            self.printcard_file = pdf_path
+            pdf_path = generate_pdf_for_printcard(printcard=self.name, pdf_path=True)
 
+            if pdf_path:
+                self.printcard_file = pdf_path
+
+                frappe.msgprint(
+                    f"El archivo PDF del PrintCard ha sido generado satisfactoriamente.",
+                    alert=True,
+                )
+
+        # ToDo: double check why this is necessary
         if codigo := frappe.db.get_value("Producto del Cliente", {
             "nombre_arte": self.nombre_arte,
         }, ["codigo"]):
             self.codigo = codigo
 
+
+    def save_arte(self):
+        arte = self._get_arte()
         arte.flags.ignore_permissions = True
         arte.flags.ignore_mandatory = True
         arte.save()
