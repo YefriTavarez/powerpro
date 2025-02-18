878e7c01df20343c7d3c336579e9f47e0a78de38
Yefri Tavarez <yefritavarez@gmail.com>
2025-02-18 11:58:04 -0500

    feat: Add DGII settings retrieval in boot session for improved configuration management

--- powerpro/boot.py
+++ powerpro/boot.py
@@ -3,10 +3,10 @@
 
 import frappe
 
-def boot_session(bootinfo):
-	settings = get_powerpro_settings()
 
-	bootinfo.powerpro_settings = settings
+def boot_session(bootinfo):
+	bootinfo.powerpro_settings = get_powerpro_settings()
+	bootinfo.dgii_settings = get_dgii_settings()
 
 
 def get_powerpro_settings():
@@ -14,4 +14,13 @@ def get_powerpro_settings():
 
 	return {
 		"root_item_group_for_raw_materials": settings.root_item_group_for_raw_materials,
+	}
+
+
+def get_dgii_settings():
+	settings = frappe.get_single("DGII Settings")
+
+	return {
+		"buying_itbis_account": settings.itbis_account,
+		"buying_itbis_rate": settings.itbis_rate,
 	}
