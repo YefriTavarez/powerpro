03ea4159beae3a8e069b4a80c5e929900bd8f2c8
Yefri Tavarez <yefritavarez@gmail.com>
2025-02-26 15:53:48 -0500

    feat: Implement automatic tax updates for items based on item group changes

--- powerpro/controllers/item.py
+++ powerpro/controllers/item.py
@@ -2,7 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import cint
+from frappe.utils import nestedset, cint
 
 
 def autoname(doc, method=None):
@@ -17,11 +17,67 @@ def autoname(doc, method=None):
 
 
 def before_save(doc, method=None):
+	autoset_item_group(doc)
+	update_item_tax(doc)
+
+
+def autoset_item_group(doc):
+	"""Will set the item_group value (the one that ERPNext knows about) based 
+	on the lowest level of the custom item group fields"""
 	doc.item_group = doc.custom_item_group_5 \
 		or doc.custom_item_group_4 \
 		or doc.custom_item_group_3 \
 		or doc.custom_item_group_2 \
 		or doc.custom_item_group_1
+
+
+def update_item_tax(doc):
+	# allow the developer to prevent the taxes from being overriden at
+	# the item level (they just need to add a dont_override_taxes field to the doc)
+	# and set it to True (or 1) to prevent the taxes from being overriden at the item level
+	dont_override_taxes = getattr(doc, "dont_override_taxes", False)
+
+	if dont_override_taxes:
+		return
+	
+	# we will use the item_group value to update the taxes.
+	# we basically use the closest parent that has taxes defined
+	# and we will copy those taxes to the item
+	item_group = frappe.get_doc("Item Group", doc.item_group)
+
+
+	if item_group.taxes:
+		doc.taxes = [] # empty only if the item_group has taxes defined
+
+		for tax in item_group.taxes:
+			doc.append("taxes", frappe.copy_doc(tax))
+	else:
+		# if the item_group doesn't have taxes defined
+		# we will check the parent item groups until we find
+		# one that has taxes defined
+		parent_item_group = get_parent_item_group(item_group.name)
+
+		while parent_item_group:
+			if parent_item_group.taxes:
+				doc.taxes = [] # clear the taxes only if we found a parent with taxes
+
+				for tax in parent_item_group.taxes:
+					doc.append("taxes", frappe.copy_doc(tax))
+
+				break
+
+			parent_item_group = get_parent_item_group(parent_item_group.name)
+
+
+def get_parent_item_group(name):
+	doctype = "Item Group"
+
+	parent = frappe.get_value(doctype, name, "parent_item_group")
+
+	if parent:
+		return frappe.get_doc(doctype, parent)
+	
+	return None
 
 
 def get_next_value(doc):
@@ -81,7 +137,6 @@ def get_last_value(serie):
 			name Rlike "{serie}-[0-9]+"
 	"""
 
-	frappe.errprint(query)
 	result = frappe.db.sql_list(query, debug=True)
 
 	if result:
---
+++ powerpro/controllers/item_group.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025, Yefri Tavarez and Contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.utils import nestedset
+
+def on_update(doc, method=None):
+	queue_update_of_items(doc)
+
+
+def queue_update_of_items(doc):
+	"""Will update the Item Tax in all Items related to this Item Group"""
+	frappe.enqueue(
+		method="powerpro.controllers.item_group.update_item_tax_of_items",
+		queue="long",
+		timeout=1500,
+		item_group_id=doc.name,
+		enqueue_after_commit=True,
+	)
+
+
+def update_item_tax_of_items(item_group_id):
+	"""Will update the Item Tax in all Items related to this Item Group"""
+	doctype = "Item Group"
+	
+	out = [item_group_id] + nestedset.get_descendants_of(
+		doctype, item_group_id
+	)
+
+	item_group = get_item_group(item_group_id)
+
+	for item_id in frappe.get_all("Item", filters={"item_group": ("in", out)}, pluck="name"):
+		item = frappe.get_doc("Item", item_id)
+		item.taxes = []
+
+		for tax in item_group.taxes:
+			item.append("taxes", frappe.copy_doc(tax))
+
+		item.save(ignore_permissions=True)
+	
+
+def get_item_group(item_group_id):
+	return frappe.get_doc("Item Group", item_group_id)
