3ff33f491bbfc4b11026c17e54675fffef3704d6
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-21 08:53:54 -0400

    refactor: Improve code formatting and consistency in arte_original controller

--- powerpro/controllers/arte_original.py
+++ powerpro/controllers/arte_original.py
@@ -8,117 +8,115 @@ from frappe.model.document import Document
 
 
 class ArteOriginal(Document):
-	def on_update(self):
-		rename_files_in_versiones_arte(self)
-		sync_with_producto_del_cliente(self)
+    def on_update(self):
+        rename_files_in_versiones_arte(self)
+        sync_with_producto_del_cliente(self)
 
 
 def sync_with_producto_del_cliente(doc):
-	product = _get_producto_del_cliente(doc.nombre_arte)
+    product = _get_producto_del_cliente(doc.nombre_arte)
 
-	if product:
-		product.possible_items = [
-			frappe.copy_doc(item)
-			for item in doc.items
-		]
+    if product:
+        product.possible_items = [frappe.copy_doc(item) for item in doc.items]
 
-		product.flags.ignore_permissions = True
-		product.flags.ignore_mandatory = True
-		product.save()
+        product.flags.ignore_permissions = True
+        product.flags.ignore_mandatory = True
+        product.save()
 
 
 def rename_files_in_versiones_arte(doc):
-	updates_count = 0
-	for row in getattr(doc, "versiones_arte", []):
-		if not row.arte:
-			continue # no file to rename
+    updates_count = 0
+    for row in getattr(doc, "versiones_arte", []):
+        if not row.arte:
+            continue  # no file to rename
 
-		filename = f"[AO]{doc.codigo_arte}.v{row.idx}.pdf"
+        filename = f"[AO]{doc.codigo_arte}.v{row.idx}.pdf"
 
-		if filename in row.arte:
-			continue # the current file is already named correctly
+        if filename in row.arte:
+            continue  # the current file is already named correctly
 
-		# let's rename the file in the file system
-		filepath = _rename_file(doc, row.arte, filename)
+        # let's rename the file in the file system
+        filepath = _rename_file(doc, row.arte, filename)
 
-		_update_file_refs(doc, row.arte, filepath)
+        _update_file_refs(doc, row.arte, filepath)
 
-		# let's update the row in the database
-		row.arte = filepath
+        # let's update the row in the database
+        row.arte = filepath
 
-		updates_count += 1
+        updates_count += 1
 
-	if updates_count:
-		_silently_doc_update(doc)
+    if updates_count:
+        _silently_doc_update(doc)
+
 
 def _get_producto_del_cliente(name):
-	doctype = "Producto del Cliente"
+    doctype = "Producto del Cliente"
 
-	if name := frappe.db.exists(doctype, {"nombre_arte": name}):
-		return frappe.get_doc(doctype, name)
-	else:
-		return None
+    if name := frappe.db.exists(doctype, {"nombre_arte": name}):
+        return frappe.get_doc(doctype, name)
+    else:
+        return None
 
 
 def _rename_file(doc, old_filepath, new_filename):
-	# old_filepath is a path /private/files/abc.pdf
-	# new_filename is a filename cde.pdf
-	# we need to keep the same path, but change the filename
-	# /private/files/cde.pdf
+    # old_filepath is a path /private/files/abc.pdf
+    # new_filename is a filename cde.pdf
+    # we need to keep the same path, but change the filename
+    # /private/files/cde.pdf
 
-	# get the base path
-	base_path = frappe.utils.get_site_path()
+    # get the base path
+    base_path = frappe.utils.get_site_path()
 
-	if old_filepath.startswith("/files"):
-		old_filepath = f"/public{old_filepath}"
+    if old_filepath.startswith("/files"):
+        old_filepath = f"/public{old_filepath}"
 
-	# check if the file is private
-	path, old_filename = old_filepath.rsplit("/", 1)
+    # check if the file is private
+    path, old_filename = old_filepath.rsplit("/", 1)
 
-	# # do the renaming
-	# shutil.move(
-	# 	f"{base_path}{old_filepath}",
-	# 	f"{base_path}/public/files/{new_filename}",
-	# )
+    # # do the renaming
+    # shutil.move(
+    # 	f"{base_path}{old_filepath}",
+    # 	f"{base_path}/public/files/{new_filename}",
+    # )
 
-	# copy instead
-	shutil.copy(
-		f"{base_path}{old_filepath}",
-		f"{base_path}/public/files/{new_filename}",
-	)
+    # copy instead
+    shutil.copy(
+        f"{base_path}{old_filepath}",
+        f"{base_path}/public/files/{new_filename}",
+    )
 
-	return f"/files/{new_filename}"
+    return f"/files/{new_filename}"
 
 
 def _update_file_refs(doc, old_filepath, new_filepath):
-	# We need to update the file references in the database
-	doctype = "File"
-	fieldname = "file_url"
+    # We need to update the file references in the database
+    doctype = "File"
+    fieldname = "file_url"
 
-	frappe.db.sql(
-		f"""
-		UPDATE
-			`tab{doctype}`
-		SET
-			`{fieldname}` = "{new_filepath}",
-			attached_to_doctype = "Arte Original",
-			attached_to_name = "{doc.name}"
-		WHERE
-			`{fieldname}` = "{old_filepath}"
-			And IfNull(`attached_to_doctype`, "") = ""
-			And IfNull(`attached_to_name`, "") = ""
-		"""
-	)
+    frappe.db.sql(
+        f"""
+        UPDATE
+            `tab{doctype}`
+        SET
+            `{fieldname}` = "{new_filepath}",
+            attached_to_doctype = "Arte Original",
+            attached_to_name = "{doc.name}"
+        WHERE
+            `{fieldname}` = "{old_filepath}"
+            And IfNull(`attached_to_doctype`, "") = ""
+            And IfNull(`attached_to_name`, "") = ""
+        """
+    )
 
 
 def _silently_doc_update(doc):
-	for child in doc.get_all_children():
-		child.db_update()
-	
-	doc.db_update()
-	doc.notify_update()
+    for child in doc.get_all_children():
+        child.db_update()
 
-	frappe.msgprint(
-		f"Arte Original {doc.name} actualizado correctamente",
-		alert=True,
-	)
+    doc.db_update()
+    doc.notify_update()
+
+    frappe.msgprint(
+        f"Arte Original {doc.name} actualizado correctamente",
+        alert=True,
+    )
