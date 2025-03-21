53e32a0a65b876420a867f4841ea9be87192fce7
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-21 08:49:34 -0400

    refactor: Move file renaming and updating logic to standalone functions for better modularity

--- powerpro/controllers/arte_original.py
+++ powerpro/controllers/arte_original.py
@@ -9,89 +9,89 @@ from frappe.model.document import Document
 
 class ArteOriginal(Document):
 	def on_update(self):
-		self.rename_files_in_versiones_arte()
+		rename_files_in_versiones_arte(self)
 	
-	def rename_files_in_versiones_arte(self):
-		updates_count = 0
-		for row in getattr(self, "versiones_arte", []):
-			if not row.arte:
-				continue # no file to rename
+def rename_files_in_versiones_arte(doc):
+	updates_count = 0
+	for row in getattr(doc, "versiones_arte", []):
+		if not row.arte:
+			continue # no file to rename
 
-			filename = f"[AO]{self.codigo_arte}.v{row.idx}.pdf"
+		filename = f"[AO]{doc.codigo_arte}.v{row.idx}.pdf"
 
-			if filename in row.arte:
-				continue # the current file is already named correctly
+		if filename in row.arte:
+			continue # the current file is already named correctly
 
-			# let's rename the file in the file system
-			filepath = self.rename_file(row.arte, filename)
+		# let's rename the file in the file system
+		filepath = doc.rename_file(row.arte, filename)
 
-			self.update_file_refs(row.arte, filepath)
+		doc.update_file_refs(row.arte, filepath)
 
-			# let's update the row in the database
-			row.arte = filepath
+		# let's update the row in the database
+		row.arte = filepath
 
-			updates_count += 1
+		updates_count += 1
 
-		if updates_count:
-			self.silently_self_update()
+	if updates_count:
+		doc.silently_doc_update()
+
+def rename_file(doc, old_filepath, new_filename):
+	# old_filepath is a path /private/files/abc.pdf
+	# new_filename is a filename cde.pdf
+	# we need to keep the same path, but change the filename
+	# /private/files/cde.pdf
+
+	# get the base path
+	base_path = frappe.utils.get_site_path()
+
+	if old_filepath.startswith("/files"):
+		old_filepath = f"/public{old_filepath}"
+
+	# check if the file is private
+	path, old_filename = old_filepath.rsplit("/", 1)
+
+	# # do the renaming
+	# shutil.move(
+	# 	f"{base_path}{old_filepath}",
+	# 	f"{base_path}/public/files/{new_filename}",
+	# )
+
+	# copy instead
+	shutil.copy(
+		f"{base_path}{old_filepath}",
+		f"{base_path}/public/files/{new_filename}",
+	)
+
+	return f"/files/{new_filename}"
+
+def update_file_refs(doc, old_filepath, new_filepath):
+	# We need to update the file references in the database
+	doctype = "File"
+	fieldname = "file_url"
+
+	frappe.db.sql(
+		f"""
+		UPDATE
+			`tab{doctype}`
+		SET
+			`{fieldname}` = "{new_filepath}",
+			attached_to_doctype = "Arte Original",
+			attached_to_name = "{doc.name}"
+		WHERE
+			`{fieldname}` = "{old_filepath}"
+			And IfNull(`attached_to_doctype`, "") = ""
+			And IfNull(`attached_to_name`, "") = ""
+		"""
+	)
+
+def silently_doc_update(doc):
+	for child in doc.get_all_children():
+		child.db_update()
 	
-	def rename_file(self, old_filepath, new_filename):
-		# old_filepath is a path /private/files/abc.pdf
-		# new_filename is a filename cde.pdf
-		# we need to keep the same path, but change the filename
-		# /private/files/cde.pdf
+	doc.db_update()
+	doc.notify_update()
 
-		# get the base path
-		base_path = frappe.utils.get_site_path()
-
-		if old_filepath.startswith("/files"):
-			old_filepath = f"/public{old_filepath}"
-
-		# check if the file is private
-		path, old_filename = old_filepath.rsplit("/", 1)
-
-		# # do the renaming
-		# shutil.move(
-		# 	f"{base_path}{old_filepath}",
-		# 	f"{base_path}/public/files/{new_filename}",
-		# )
-
-		# copy instead
-		shutil.copy(
-			f"{base_path}{old_filepath}",
-			f"{base_path}/public/files/{new_filename}",
-		)
-
-		return f"/files/{new_filename}"
-
-	def update_file_refs(self, old_filepath, new_filepath):
-		# We need to update the file references in the database
-		doctype = "File"
-		fieldname = "file_url"
-
-		frappe.db.sql(
-			f"""
-			UPDATE
-				`tab{doctype}`
-			SET
-				`{fieldname}` = "{new_filepath}",
-				attached_to_doctype = "Arte Original",
-				attached_to_name = "{self.name}"
-			WHERE
-				`{fieldname}` = "{old_filepath}"
-				And IfNull(`attached_to_doctype`, "") = ""
-				And IfNull(`attached_to_name`, "") = ""
-			"""
-		)
-
-	def silently_self_update(self):
-		for child in self.get_all_children():
-			child.db_update()
-		
-		self.db_update()
-		self.notify_update()
-
-		frappe.msgprint(
-			f"Arte Original {self.name} actualizado correctamente",
-			alert=True,
-		)
+	frappe.msgprint(
+		f"Arte Original {doc.name} actualizado correctamente",
+		alert=True,
+	)
