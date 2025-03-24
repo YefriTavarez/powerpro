e5bbb6b89df5ae9aee0ced6e824ba3a057d67e88
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-23 21:38:36 -0400

    refactor: Move get_project_fields function outside of ProjectTemplate class for better modularity

--- powerpro/controllers/project_template.py
+++ powerpro/controllers/project_template.py
@@ -16,46 +16,46 @@ class ProjectTemplate(project_template.ProjectTemplate):
 
 	def set_project_docfields(self):
 		if not self.project_docfields:
-			for df in self.get_project_fields():
+			for df in get_project_fields():
 				self.append("project_docfields", {
 					"label": frappe._(df.label, "es"),
 					"fieldname": df.fieldname,
 				})
 
-	def get_project_fields(self):
-		meta = frappe.get_meta("Project")
+def get_project_fields():
+	meta = frappe.get_meta("Project")
 
-		# only value fields
-		value_fields = [
-			field
-			for field in meta.fields
-			if field.fieldtype not in no_value_fields
-		]
+	# only value fields
+	value_fields = [
+		field
+		for field in meta.fields
+		if field.fieldtype not in no_value_fields
+	]
 
-		# get reqd fields
-		reqd_fields = [
-			field
-			for field in value_fields
-			if field.reqd
-		]
+	# get reqd fields
+	reqd_fields = [
+		field
+		for field in value_fields
+		if field.reqd
+	]
 
 
-		# get read_only fields
-		read_only_fields = [
-			field
-			for field in value_fields
-			if field.read_only
-		]
+	# get read_only fields
+	read_only_fields = [
+		field
+		for field in value_fields
+		if field.read_only
+	]
 
-		# exclude reqd and read_only fields
-		exclude_fields = reqd_fields + read_only_fields
+	# exclude reqd and read_only fields
+	exclude_fields = reqd_fields + read_only_fields
 
-		# get project fields
-		return [
-			field
-			for field in value_fields
-			if field not in exclude_fields
-		]
+	# get project fields
+	return [
+		field
+		for field in value_fields
+		if field not in exclude_fields
+	]
 
 
 @frappe.whitelist()
