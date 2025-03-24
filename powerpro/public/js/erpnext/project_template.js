4de3632c804db16274fbeae820d18d620f4b95eb
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-24 17:27:26 -0400

    feat: Add custom buttons for synchronizing and resetting project document fields

--- powerpro/public/js/erpnext/project_template.js
+++ powerpro/public/js/erpnext/project_template.js
@@ -4,11 +4,69 @@
 
 {
 	function refresh(frm) {
+		_add_custom_buttons(frm);
 		_setup_docfields_table_as_readonly(frm);
 	}
 
 	function onload(frm) {
 		_dirty_form_if_new_docfields(frm);
+	}
+
+	function _add_custom_buttons(frm) {
+		// const actions_parent = __("Actions");
+		const docfields_parent = __("DocFields");
+
+		frappe.run_serially([
+			() => {
+				const label = __("Syncronizar");
+				const action = () => {
+					const method = "set_project_docfields";
+					const args = {
+						for_reload: true,
+						with_memory: true
+					};
+					function callback(response) {
+						frappe.show_alert({
+							message: __("Campos de projecto sincronizados"),
+							indicator: "green",
+						});
+					}
+
+					frm.call(method, args, callback);
+				};
+
+				frm.add_custom_button(label, action, docfields_parent);
+			},
+			() => {
+				const label = __("Resetear");
+				const action = () => {
+					frappe.confirm(
+						__("¿Estás seguro de que deseas resetear los campos de proyecto?"),
+						() => _reset_project_docfields(frm), () => {
+							frappe.show_alert({
+								message: __("Operación cancelada"),
+								indicator: "red",
+							});
+						}
+					);
+				};
+
+				frm.add_custom_button(label, action, docfields_parent);
+			},
+		]);
+	}
+
+	function _reset_project_docfields(frm) {
+		const { doc } = frm;
+		for (
+			const child of doc.project_docfields
+		) {
+			child.hidden = false;
+			child.reqd = false;
+			child.read_only = false;
+		}
+
+		frappe.run_serially([ frm.dirty.bind(frm), frm.save.bind(frm) ]);
 	}
 
 	function _setup_docfields_table_as_readonly(frm) {
