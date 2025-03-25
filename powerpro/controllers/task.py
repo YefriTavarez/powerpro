6f87c37e2fbd7d91e17375c7999816d0cf26fbdb
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-25 00:28:20 -0400

    refactor: Remove validation for responsable in Task class

--- powerpro/controllers/task.py
+++ powerpro/controllers/task.py
@@ -8,14 +8,4 @@ from erpnext.projects.doctype.task import task
 
 
 class Task(task.Task):
-    def validate(self):
-        super().validate()
-
-        self.check_responsable_is_set()
-    
-    def check_responsable_is_set(self):
-        if self.is_template:
-            return
-
-        if not self.responsable:
-            frappe.throw("Responsable is required")
+    ...
