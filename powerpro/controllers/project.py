a8c4fa3af0467dd596c43b20d53158dff4a6928c
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-26 22:48:08 -0400

    feat: Implement working date calculation with holiday consideration in project tasks

--- powerpro/controllers/project.py
+++ powerpro/controllers/project.py
@@ -1,10 +1,11 @@
 # Copyright (c) 2025, Yefri Tavarez and Contributors
 # For license information, please see license.txt
 
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Literal, Union
 
 if TYPE_CHECKING:
     from frappe.model import document
+    import datetime
 
 import frappe
 from frappe.utils import today, cint
@@ -86,6 +87,26 @@ class Project(project.Project):
             self.last_task_end_date, minutes=gap_in_minutes
         )
 
+        if department := project_template_task.department:
+            _, holiday_list = get_shift_details(department)
+
+            # if shift_type:
+            #     start_time, end_time = frappe.get_value(
+            #         "Shift Type", shift_type, ["start_time", "end_time"]
+            #     )
+
+            #     # # need to validate if the start time of the task is within the shift timings
+            #     # # if not, then adjust the start time to the next working day (using holiday list)
+            #     # if task_expected_start_date.strftime("%H:%M") < start_time:
+            #     #     task_expected_start_date = frappe.utils.add_to_date(
+            #     #         task_expected_start_date, days=1
+            #     #     )
+
+            # PS: the start and end date fields on the Task are date fields and not datetime fields...
+            # so, let's keep it simple for now
+            task_expected_start_date = get_working_date_or_next(task_expected_start_date, holiday_list=holiday_list)
+
+
         task_expected_end_date = frappe.utils.add_to_date(
             self.last_task_end_date, minutes=project_template_task.get_duration_in_minutes()
         )
@@ -166,3 +187,26 @@ class Project(project.Project):
                     out.append(responsible)
 
         return out
+
+
+def get_shift_details(department_id):
+    doctype = "Department"
+    return frappe.get_value(doctype, department_id, ["shift_type", "holiday_list"])
+
+
+def get_working_date_or_next(date: Union["datetime.date", "datetime.datetime"], holiday_list: str=None) -> "datetime.date":
+    while not is_working_date(date, holiday_list=holiday_list):
+        date = frappe.utils.add_to_date(date, days=1)
+    return date
+
+
+def is_working_date(date: Union["datetime.date", "datetime.datetime"], holiday_list: str=None) -> bool:
+    doctype = "Holiday"
+    filters = dict(
+        holiday_date=date
+    )
+
+    if holiday_list:
+        filters["parent"] = holiday_list
+
+    return frappe.db.exists(doctype, filters) is None
