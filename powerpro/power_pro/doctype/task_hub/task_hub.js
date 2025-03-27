fe44b2bfb6a7c0741d8c77066528fbc6634ea31f
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-27 01:16:45 -0400

    feat: Enhance Task Hub with default setup, task rendering, and new UI elements

--- powerpro/power_pro/doctype/task_hub/task_hub.js
+++ powerpro/power_pro/doctype/task_hub/task_hub.js
@@ -3,7 +3,16 @@
 
 {
 	const { datetime: date } = frappe;
+	function setup(frm) {
+		_disable_save(frm);
+	}
+
 	function onload_post_render(frm) {
+		_set_defaults(frm);
+		_render_task_list(frm);
+	}
+
+	function _set_defaults(frm) {
 		const { doc } = frm;
 
 		if (!doc.responsible) {
@@ -17,11 +26,31 @@
 		if (!doc.to_date) {
 			frm.set_value("to_date", date.add_days(doc.from_date, 30));
 		}
+	}
 
+	function _render_task_list(frm) {
+		const field = frm.get_field("hub");
+
+		field
+			.$wrapper
+			.empty()
+			.html(
+				frappe.render_template(
+					"task_list",
+					{
+						"tasks": frm.doc.tasks || [],
+					}
+				)
+			)
+		;
+	}
+
+	function _disable_save(frm) {
 		frm.disable_save();
 	}
 
 	frappe.ui.form.on("Task Hub", {
+		setup,
 		onload_post_render,
 	});
 }
