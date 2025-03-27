5c4f9cc629ef097697a745ab220072463c6ecbff
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-27 00:39:46 -0400

    feat: Implement onload_post_render function to set default values and disable save in Task Hub

--- powerpro/power_pro/doctype/task_hub/task_hub.js
+++ powerpro/power_pro/doctype/task_hub/task_hub.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2025, Yefri Tavarez and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Task Hub", {
-// 	refresh(frm) {
+{
+	const { datetime: date } = frappe;
+	function onload_post_render(frm) {
+		const { doc } = frm;
 
-// 	},
-// });
+		if (!doc.responsible) {
+			frm.set_value("responsible", frappe.session.user);
+		}
+
+		if (!doc.from_date) {
+			frm.set_value("from_date", date.nowdate());
+		}
+
+		if (!doc.to_date) {
+			frm.set_value("to_date", date.add_days(doc.from_date, 30));
+		}
+
+		frm.disable_save();
+	}
+
+	frappe.ui.form.on("Task Hub", {
+		onload_post_render,
+	});
+}
