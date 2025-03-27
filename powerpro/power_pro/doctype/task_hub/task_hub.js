ca9492e155a774970a14a8eaa3d74b40f4828518
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-27 00:27:02 -0400

    feat: Add Task Hub DocType with initial structure and test cases

---
+++ powerpro/power_pro/doctype/task_hub/task_hub.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Yefri Tavarez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Task Hub", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ powerpro/power_pro/doctype/task_hub/task_hub.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2025, Yefri Tavarez and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TaskHub(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		from_date: DF.Date | None
+		responsible: DF.Link | None
+		status: DF.Literal["Open", "Working", "Overdue", "Pending Review", "Completed", "Cancelled"]
+		to_date: DF.Date | None
+	# end: auto-generated types
+	
+	def db_insert(self, *args, **kwargs):
+		pass
+
+	def load_from_db(self):
+		pass
+
+	def db_update(self):
+		pass
+
+	@staticmethod
+	def get_list(args):
+		pass
+
+	@staticmethod
+	def get_count(args):
+		pass
+
+	@staticmethod
+	def get_stats(args):
+		pass
+
+	_table_fieldnames: list[str] = []
