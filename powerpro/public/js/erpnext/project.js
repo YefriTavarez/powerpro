31e11c143fff792d008dd794a72ea26e5e52e3b4
Yefri Tavarez <yefritavarez@gmail.com>
2025-04-13 20:34:12 -0400

    fix: Correct debounce implementation in field listener to ensure proper project name updates

--- powerpro/public/js/erpnext/project.js
+++ powerpro/public/js/erpnext/project.js
@@ -34,15 +34,15 @@ frappe.provide("powerpro.masks");
 		frm.set_value("producto", "");
 	}
 
-	const listen_on_field = frappe.utils.debounce(function(frm, fieldname) {
-		frappe.ui.form.on(frm.doctype, fieldname, function(frm) {
+	const listen_on_field = function(frm, fieldname) {
+		frappe.ui.form.on(frm.doctype, fieldname, frappe.utils.debounce(function(frm) {
 			if (last_value[fieldname] === frm.doc[fieldname]) {
 				return;
 			}
 			last_value[fieldname] = frm.doc[fieldname];
 			_update_project_name({ frm, fieldname });
-		});
-	}, 1000);
+		}), 1000);
+	}
 
 	function _listen_on_all_fields(frm) {
 		const { fields } = frm;
