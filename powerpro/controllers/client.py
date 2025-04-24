9aad95abb4140dcc836eec1b86be40f8955966ee
Yefri Tavarez <yefritavarez@gmail.com>
2025-04-24 13:33:57 -0400

    fix: remove s after join

--- powerpro/controllers/client.py
+++ powerpro/controllers/client.py
@@ -41,7 +41,7 @@ def get_producto_del_cliente(texto_busqueda=None, item_group=None):
         LEFT JOIN
             `tabMultiple Items` AS possible_items
             ON item.name = possible_items.item
-        LEFT JOINs
+        LEFT JOIN
             `tabProducto del Cliente` AS product
             ON
                 possible_items.parent = product.name
