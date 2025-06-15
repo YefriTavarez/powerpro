182f4535a4d73cf19a574a9eafefff97e6db5dfe
Yefri Tavarez <yefritavarez@gmail.com>
2025-06-14 21:12:06 -0400

    feat: Add roll conversion order settings retrieval to powerpro settings

--- powerpro/boot.py
+++ powerpro/boot.py
@@ -14,6 +14,7 @@ def get_powerpro_settings():
 	return {
 		"root_item_group_for_raw_materials": settings.root_item_group_for_raw_materials,
 		"project_manager": get_project_manager(),
+		"roll_conversion_order_settings": get_roll_conversion_order_settings(),
 	}
 
 
@@ -22,3 +23,13 @@ def get_project_manager():
 	settings = frappe.get_single("Projects Settings")
 
 	return settings.project_manager or "System Manager"
+
+
+def get_roll_conversion_order_settings():
+	"""Get the roll conversion order settings from the settings"""
+	settings = frappe.get_single("Roll Conversion Order Settings")
+
+	return {
+		"default_conversion_source_warehouse": settings.default_conversion_source_warehouse,
+		"default_conversion_target_warehouse": settings.default_conversion_target_warehouse,
+	}
