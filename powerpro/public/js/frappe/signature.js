7570b6b3e8d556746d08fd47bf2b87b276b31113
Yefri Tavarez <yefritavarez@gmail.com>
2025-06-23 12:54:41 -0400

    feat: Add reset functionality to signature control

--- powerpro/public/js/frappe/signature.js
+++ powerpro/public/js/frappe/signature.js
@@ -35,3 +35,13 @@ frappe.ui.form.ControlSignature.prototype.make_pad = function() {
 		this.refresh_input();
 	}
 };
+
+frappe.ui.form.ControlSignature.prototype.on_reset_sign = function() {
+	this.$pad.jSignature("reset");
+	this.set_my_value("");
+
+	const value = this.get_value();
+	if (value) {
+		this.set_model_value("");
+	}
+}
