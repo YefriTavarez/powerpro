5fd193b6bd39c6f58a814ccd14443b345488aeab
Yefri Tavarez <yefritavarez@gmail.com>
2026-02-09 16:34:35 -0400

    Refactor filtering logic in open_projects_multiselect function to enhance query construction
    - Improved the dynamic filtering mechanism by consolidating conditions for project selection based on user input for sales orders, SKUs, and arte.
    - Cleaned up the code for better readability by removing unnecessary commented-out sections and optimizing the filter return structure.
    - Ensured that the filters are constructed based on the dialog values for a more efficient data retrieval process.

--- powerpro/public/js/delivery_note.js
+++ powerpro/public/js/delivery_note.js
@@ -41,23 +41,35 @@ function open_projects_multiselect(frm, target_type) {
                         ['Sales Order', 'customer', '=', frm.doc.customer || ""],
                     ].concat(frm.doc.company ? [['Sales Order', 'company', '=', frm.doc.company]] : [])
                 };
-            } },
+            }, 
+                // change() {
+                //     const { value } = this;
+                //     d.get_results();
+                //     // d.show_child_results();
+                // } 
+            },
             { fieldname: 'sku_producto', fieldtype: 'Link', options: 'Item', label: __('SKU Producto') },
             { fieldname: 'arte', fieldtype: 'Link', options: 'Arte', label: __('Arte'), get_query() {
                 return {
                     filters: [
-                        ['Arte', ' cliente', '=', frm.doc.customer || ""],
-                    ].concat(frm.doc.company ? [['Arte', 'company', '=', frm.doc.company]] : [])
+                        ['Arte', 'cliente', '=', frm.doc.customer || ""],
+                    ]
                 };
             } },
         ],
         get_query() {
-            return {
-                filters: [
-                    ['Project', 'sales_order', 'is', 'set'],
-                    ['Project', 'customer', '=', frm.doc.customer || ""],
-                ].concat(frm.doc.company ? [['Project','company','=','' + frm.doc.company]] : [])
-            };
+            const { dialog } = this;
+            const filters = [
+                ['Project', 'customer', '=', frm.doc.customer || ""],
+                ['Project', 'company', '=', frm.doc.company || ""],
+            ];
+            
+            if (dialog.get_value("sales_order")) filters.push(['Project', 'sales_order', '=', dialog.get_value("sales_order")]);
+            if (dialog.get_value("sku_producto")) filters.push(['Project', 'sku_producto', '=', dialog.get_value("sku_producto")]);
+            if (dialog.get_value("arte")) filters.push(['Project', 'arte', '=', dialog.get_value("arte")]);
+
+
+            return { filters };
         },
         size: 'large',
         action(selections) {
@@ -75,10 +87,9 @@ function open_projects_multiselect(frm, target_type) {
                 const errors = data.errors || [];
 
                 for (const it of items) {
-                    const row = frm.add_child('items');
-                    Object.assign(row, it);
+                    const row = frm.add_child('items', it);
+                    // Object.assign(row, it);
                 }
-
                 frm.refresh_field('items');
                 if (errors.length) {
                     frappe.msgprint({
